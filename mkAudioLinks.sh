4f56f9fedbc43858e60304127542a9946f822e58
Kobi Kai Calev <kcalev@gmail.com>
2017-12-31 22:38:23 +0200

    renderMock starts

--- mkAudioLinks.sh
+++ mkAudioLinks.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+mkdir ./transparent-playlists
+mkdir ./transparent-html
+
 cd ./transparent-sound/
 for lang in * 
 do
     cd ../transparent-playlists
+    ln -s ../transparent-sound/$lang $lang
+    cd ../transparent-html/
     ln -s ../transparent-sound/$lang $lang
 done
 
---
+++ transparent-mk-html.jl
@@ -0,0 +1,23 @@
+#!/usr/bin/env julia
+
+using Glob
+using JSON
+
+lang = "russian"
+
+o_list = cd("transparent-download/$lang") do
+    j_files = glob("*.json")
+    o_list = map(j_files) do fn
+        o = JSON.parsefile(fn)
+        o["word"], o["fnphrase"]
+        o["wotd:transliteratedWord"]
+        o["wotd:transliteratedSentence"]
+        o
+    end
+    return o_list
+end
+
+
+
+
+
