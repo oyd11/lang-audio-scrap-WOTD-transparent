fc39641323698a1218e5e1b83c9272706eee2daf
Kobic <kobic@voiceitt.com>
2018-01-11 13:42:17 +0200

    bugfix

--- render/script02.js
+++ render/script02.js
@@ -1,7 +1,6 @@
 
-// utils: 
 function existsNotEmpty(w) {
-    return  null != w && !isempty(w) 
+    return null != w && !isempty(w) 
 }
 
 // assumed: defined 'j' - JSON + 'lang' - language string
@@ -65,7 +64,8 @@ function addEventsAndPlay(ind) {
         w.style.fontSize='110%'
         ww.style.color='red'
         ww.style.fontSize='135%'
-        if (existsNotEmpty(wwt)) {
+        if (null != wwt) {
+            console.log("WWT: Red")
             wwt.style.color='red'
             wwt.style.fontSize='135%'
         }
@@ -74,7 +74,7 @@ function addEventsAndPlay(ind) {
     function onEnded(ev) { 
         w.style = null
 //        ww.style = null
-        if (existsNotEmpty(wwt)) {
+        if (null != wwt) {
  //       wwt.style = null
         }
         rmEvents()
@@ -121,88 +121,86 @@ function isempty(obj) {
 }
 
 sentenceList.forEach( (w,ind) => {
-    const wordDiv = document.createElement("div")
-    divWordList.appendChild(wordDiv)
-    const wordUl = document.createElement("ul")
-    wordDiv.appendChild(wordUl)
-    wordDiv.setAttribute("id","wd_"+ind)
-    wordUl.setAttribute("id","w_"+ind)
-{
-    const newLi= document.createElement("li")
-    const wt = w["wotd:transliteratedWord"] 
-    const wt_text = isempty(wt) ? "" : `(${wt})`
-    newLi.textContent = `${w["word"]} ${wt_text} :: ${w["translation"]} (${w["wordtype"]})`
-    wordUl.appendChild(newLi)
-}
-{
-    const newLi= document.createElement("li")
-    newLi.textContent = w["fnphrase"]
-    newLi.setAttribute("id","ww_"+ind)
-    wordUl.appendChild(newLi)
-}
-const wwt = w["wotd:transliteratedSentence"]
-
-
-if (existsNotEmpty(wwt)){
-    const newLi= document.createElement("li")
-    newLi.textContent = wwt
-    newLi.setAttribute("id","wwt_"+ind)
-    wordUl.appendChild(newLi)
-}
-{
-    const newLi= document.createElement("li")
-    newLi.textContent = w["enphrase"]
-    wordUl.appendChild(newLi)
-}
-{
-    const au = document.createElement("audio")
-    au.setAttribute("id","au_"+ind)
-    au.setAttribute("src",
-        `../transparent-sound/${lang}-sound/${w["date"]}-phrasesound.mp3`)
-//    au.controls = true
-    au.preload="none" // "auto|metadata|none">
-    wordUl.appendChild(au)
-}
-{
-    const audioElement = document.createElement("audio")
-    audioElement.setAttribute("id","auw_"+ind)
-    audioElement.setAttribute("src",
-        `../transparent-sound/${lang}-sound/${w["date"]}-wordsound.mp3`)
- //   audioElement.controls = true
-    wordUl.appendChild(audioElement)
-}
-{
-    const newLi= document.createElement("li")
-    newLi.textContent = w["date"]
-    newLi.style.fontSize="60%"
-    wordUl.appendChild(newLi)
-}
-{
-    function mDownPlayThis(ev) {
-        wordDiv.style.color='red'
-        if (null != curPlaying) {
-            curPlaying.pause()
-            curPlaying.currentTime = 0
-        }
-//        curPlaying = au
-        addEventsAndPlay(ind)
+        const wordDiv = document.createElement("div")
+        divWordList.appendChild(wordDiv)
+        const wordUl = document.createElement("ul")
+        wordDiv.appendChild(wordUl)
+        wordDiv.setAttribute("id","wd_"+ind)
+        wordUl.setAttribute("id","w_"+ind)
+    {
+        const newLi= document.createElement("li")
+        const wt = w["wotd:transliteratedWord"] 
+        const wt_text = isempty(wt) ? "" : `(${wt})`
+        newLi.textContent = `${w["word"]} ${wt_text} :: ${w["translation"]} (${w["wordtype"]})`
+        wordUl.appendChild(newLi)
     }
-    wordDiv.addEventListener("mouseenter", (ev) => {
-//        wordDiv.style.fontSize='150%'
-        wordDiv.style.color='aqua'
-        wordDiv.addEventListener("mousedown", mDownPlayThis)
-    })
-    wordDiv.addEventListener("mouseleave", (ev) => {
-        wordDiv.style=null
-        wordDiv.removeEventListener("mousedown", mDownPlayThis)
-    })
-}
-{
-    const newHr = document.createElement("hr")
-    wordDiv.appendChild(newHr)
-}
+    {
+        const newLi= document.createElement("li")
+        newLi.textContent = w["fnphrase"]
+        newLi.setAttribute("id","ww_"+ind)
+        wordUl.appendChild(newLi)
+    }
+    const wwt = w["wotd:transliteratedSentence"]
+    if (existsNotEmpty(wwt)){
+        const newLi= document.createElement("li")
+        newLi.textContent = wwt
+        newLi.setAttribute("id","wwt_"+ind)
+        wordUl.appendChild(newLi)
+    }
+    {
+        const newLi= document.createElement("li")
+        newLi.textContent = w["enphrase"]
+        wordUl.appendChild(newLi)
+    }
+    {
+        const au = document.createElement("audio")
+        au.setAttribute("id","au_"+ind)
+        au.setAttribute("src",
+            `../transparent-sound/${lang}-sound/${w["date"]}-phrasesound.mp3`)
+    //    au.controls = true
+        au.preload="none" // "auto|metadata|none">
+        wordUl.appendChild(au)
+    }
+    {
+        const audioElement = document.createElement("audio")
+        audioElement.setAttribute("id","auw_"+ind)
+        audioElement.setAttribute("src",
+            `../transparent-sound/${lang}-sound/${w["date"]}-wordsound.mp3`)
+     //   audioElement.controls = true
+        wordUl.appendChild(audioElement)
+    }
+    {
+        const newLi= document.createElement("li")
+        newLi.textContent = w["date"]
+        newLi.style.fontSize="60%"
+        wordUl.appendChild(newLi)
+    }
+    {
+        function mDownPlayThis(ev) {
+            wordDiv.style.color='red'
+            if (null != curPlaying) {
+                curPlaying.pause()
+                curPlaying.currentTime = 0
+            }
+    //        curPlaying = au
+            addEventsAndPlay(ind)
+        }
+        wordDiv.addEventListener("mouseenter", (ev) => {
+    //        wordDiv.style.fontSize='150%'
+            wordDiv.style.color='aqua'
+            wordDiv.addEventListener("mousedown", mDownPlayThis)
+        })
+        wordDiv.addEventListener("mouseleave", (ev) => {
+            wordDiv.style=null
+            wordDiv.removeEventListener("mousedown", mDownPlayThis)
+        })
+    }
+    {
+        const newHr = document.createElement("hr")
+        wordDiv.appendChild(newHr)
+    }
 
-})
+}) // foreach word entry
 
 const playlist = { start: 0, end: j[lang].length }
 
